53b9a29bde4237a3ab29d4e31b1e9e9820f128ec
João Vitor Lima <108885897+SenhorLime@users.noreply.github.com>
2024-08-25 20:21:53 -0300

    Game Template da LibGDX

---
+++ core/src/main/java/io/github/INFO32024/TowerDefense_T1/Main.java
@@ -0,0 +1,11 @@
+package io.github.INFO32024.TowerDefense_T1;
+
+import com.badlogic.gdx.Game;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends Game {
+    @Override
+    public void create() {
+        setScreen(new FirstScreen());
+    }
+}
