44e3b1d5f888a0966521bcea251ee723649c90e6
Wiito <tminemcraft123@gmail.com>
2024-09-03 20:01:51 -0300

    Making the code execute

--- core/src/main/java/io/github/INFO32024/TowerDefense_T1/FirstScreen.java
+++ core/src/main/java/io/github/INFO32024/TowerDefense_T1/FirstScreen.java
@@ -3,49 +3,31 @@ package io.github.INFO32024.TowerDefense_T1;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-/** First screen of the application. Displayed after the application is created. */
-public class FirstScreen implements Screen{
+/**
+ * First screen of the application. Displayed after the application is created.
+ */
+public class FirstScreen implements Screen {
 
     private AssetManager assetManager;
     private SpriteBatch batch;
-    private Texture fundo, personagemRun, personagemJump, aterrizar;
-    private Music  musica;
-    
+
+    public FirstScreen() {
+        batch = new SpriteBatch();
+        assetManager = new AssetManager();
+    }
+
     @Override
     public void show() {
         // Prepare your screen here.
     }
 
+    // ESSA FUNÇÃO TAVA AQUI SIMPLISMENTE EXISTINDO, NÃO FAZIA NADA KAKAKAKAKAKA
+    // Vou deixar pq alguem devia querer usar, se ninguem upar codio aq antes da
+    // sprint acabar, excluir
     public void create() {
-        assetManager = new AssetManager();
-        batch = new SpriteBatch();
-        
-        // Load assets
-        assetManager.load("assets/BG/BG.png", Texture.class);
-        assetManager.load("assets/png/Run(4).png", Texture.class);
-        assetManager.load("assets/png/Jump(4).png", Texture.class);
-        assetManager.load("assets/png/Jump(11).png", Texture.class);
-        assetManager.load("assets/background_music.mp3", Music.class);
-
-        // ...
-
-        // Retrieve assets
-        fundo = assetManager.get("assets/BG/BG.png", Texture.class);
-        personagemRun = assetManager.get("assets/png/Run(4).png", Texture.class);
-        personagemJump = assetManager.get("assets/png/Jump(4).png", Texture.class);
-        aterrizar = assetManager.get("assets/png/Jump(11).png", Texture.class);
-        musica = assetManager.get("assets/background_music.mp3", Music.class);
-        musica = assetManager.get("assets/background_music.mp3", Music.class);
-
-       musica.setLooping(true);
-       musica.setVolume(0.8f);
-
-       musica.play();    
     }
 
     @Override
@@ -54,10 +36,6 @@ public class FirstScreen implements Screen{
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batch.begin();
-        batch.draw(fundo, 0, 0);
-        batch.draw(personagemRun, 0, 0, 100, 100);
-        batch.draw(personagemJump, 200, 50, 100, 100);
-        batch.draw(aterrizar, 400, 0, 100, 100);
         batch.end();
     }
 
@@ -86,7 +64,6 @@ public class FirstScreen implements Screen{
         // Destroy screen's assets here.
 
         batch.dispose();
-        musica.dispose();
         assetManager.dispose();
     }
 }
